4698c27f37e4866daa9e1cf0de8568a1aa0fe407
franco2730 <franco.rosales2130@gmail.com>
2023-08-20 09:18:58 -0300

    citas-app

---
+++ citas-app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return ( //Todos los componentes / funciones deben tener un return. 
+    <div>
+        <h1>Hola mundo y vite</h1>
+        <input type="text" />
+    </div>
+  )
+}
+
+export default App
