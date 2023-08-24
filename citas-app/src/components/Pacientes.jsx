fd793b027c4b5a224e3a51f7221ea34b93e821a3
franco2730 <franco.rosales2130@gmail.com>
2023-08-24 08:39:40 -0300

    citas-app

--- citas-app/src/components/Pacientes.jsx
+++ citas-app/src/components/Pacientes.jsx
@@ -1,5 +1,6 @@
 const Pacientes = () => {
   return (
+    
     <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Nombre:
